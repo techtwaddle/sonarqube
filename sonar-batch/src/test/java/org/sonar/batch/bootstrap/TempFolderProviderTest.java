808c192be0011284083cb9a949cd8f9da8fd7235
Duarte Meneses <duarte.meneses@sonarsource.com>
2015-06-16 17:33:37 +0200

    improved tests for persistit/temp folders

--- sonar-batch/src/test/java/org/sonar/batch/bootstrap/TempFolderProviderTest.java
+++ sonar-batch/src/test/java/org/sonar/batch/bootstrap/TempFolderProviderTest.java
@@ -1,3 +1,22 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2014 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.batch.bootstrap;
 
 import org.apache.commons.io.FileUtils;
@@ -32,7 +51,7 @@ public class TempFolderProviderTest {
 
   @Test
   public void createTempFolderSonarHome() throws Exception {
-    //with sonar home, it will be in {sonar.home}/.sonartmp
+    // with sonar home, it will be in {sonar.home}/.sonartmp
     File sonarHome = temp.newFolder();
     File tmpDir = new File(new File(sonarHome, CoreProperties.GLOBAL_WORKING_DIRECTORY_DEFAULT_VALUE), TempFolderProvider.TMP_NAME);
 
@@ -48,7 +67,7 @@ public class TempFolderProviderTest {
     // if nothing is defined, it will be in {user.home}/.sonar/.sonartmp
     File defaultSonarHome = new File(System.getProperty("user.home"), ".sonar");
     File tmpDir = new File(new File(defaultSonarHome, CoreProperties.GLOBAL_WORKING_DIRECTORY_DEFAULT_VALUE), TempFolderProvider.TMP_NAME);
-    
+
     try {
       TempFolder tempFolder = tempFolderProvider.provide(new BootstrapProperties(Collections.<String, String>emptyMap()));
       tempFolder.newDir();
